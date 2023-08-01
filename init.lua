57c3f6f0b24ce504f3a097f8a07356b71663d7ce
lm <202247986@qq.com>
2023-08-01 13:22:44 +0800

    add Lazy

--- init.lua
+++ init.lua
@@ -15,8 +15,34 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 
  -- keybindings
 local opt = { noremap = true, silent = true }
-
+vim.g.mapleader = " "
 vim.keymap.set("n", "<C-l>", "<C-w>l",opt)
 vim.keymap.set("n", "<C-h>", "<C-w>h",opt)
 vim.keymap.set("n", "<C-j>", "<C-w>j",opt)
 vim.keymap.set("n", "<C-k>", "<C-w>k",opt)
+vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
+vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
+vim.keymap.set("n", "<Leader>[", "<C-o>", opt)
+vim.keymap.set("n", "<Leader>]", "<C-i>", opt)
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+	},
+})
+
+-- color scheme
+vim.cmd.colorscheme("base16-tender")
